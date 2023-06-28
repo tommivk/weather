55a0b1230c015dc5242d24da76da2e864dc33874
tommivk <tommikokk@gmail.com>
2023-06-28 13:39:05 +0300

    add commands

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"text/tabwriter"
 
 	_ "github.com/joho/godotenv/autoload"
 )
@@ -54,22 +55,21 @@ func fetchData(url string) ([]byte, error) {
 	return body, nil
 }
 
-func fetchCoordinates(city, country string) (Coordinates, error) {
+func fetchLocationData(city, country string) (GeoResponse, error) {
 	var GEO_URL = fmt.Sprintf("http://api.openweathermap.org/geo/1.0/direct?q=%s,%s&limit=1&appid=%s", city, country, API_KEY)
 
 	data, err := fetchData(GEO_URL)
 	if err != nil {
-		return Coordinates{}, err
+		return GeoResponse{}, err
 	}
 
 	var result []GeoResponse
 	json.Unmarshal(data, &result)
 	if len(result) == 0 {
-		return Coordinates{}, errors.New("Location not found")
+		return GeoResponse{}, errors.New("Location not found")
 	}
 
-	coordinates := Coordinates{Lat: result[0].Lat, Lon: result[0].Lon}
-	return coordinates, nil
+	return result[0], nil
 }
 
 type WeatherDetails struct {
@@ -126,23 +126,23 @@ func fetchWeather(coordinates Coordinates) (WeatherResult, error) {
 }
 
 func printResult(result WeatherResult) {
-	fmt.Println("--------------------------------------------------------")
 	fmt.Printf("\nWeather in %s, %s: \n\n", result.City, result.Country)
 	fmt.Printf("%s \nTemperature: %f ℃ \nFeels like: %f ℃ \n\n", strings.Title(result.Description), result.Temperature, result.FeelsLike)
 	fmt.Println("--------------------------------------------------------")
 }
 
-func fetchFavourites() {
+func fetchFavourites() error {
 	if len(config.Favourites) == 0 {
 		fmt.Println("No favourites added")
 	}
 	for i := 0; i < len(config.Favourites); i++ {
 		weather, err := fetchWeather(config.Favourites[i].Coordinates)
 		if err != nil {
-			log.Fatal(nil)
+			return err
 		}
 		printResult(weather)
 	}
+	return nil
 }
 
 func readConfigFile() error {
@@ -182,6 +182,43 @@ func saveConfig(config []byte) error {
 	return nil
 }
 
+func listFavourites() {
+	favourites := config.Favourites
+	if len(favourites) == 0 {
+		fmt.Println("No favourites added")
+	}
+	fmt.Println("\n------Favourites------")
+	for i := 0; i < len(favourites); i++ {
+		fmt.Printf("\n%s, %s", favourites[i].City, favourites[i].Country)
+	}
+	fmt.Print("\n\n----------------------\n\n")
+}
+
+func removeFavourite(city string) error {
+	favourites := config.Favourites
+	var index int = -1
+	for i := 0; i < len(favourites); i++ {
+		if strings.ToLower(favourites[i].City) == city {
+			index = i
+			break
+		}
+	}
+	if index == -1 {
+		return fmt.Errorf("City %s does not exist in favourites", city)
+	}
+	config.Favourites = append(config.Favourites[:index], config.Favourites[index+1:]...)
+	configBytes, err := json.Marshal(config)
+	if err != nil {
+		return err
+	}
+	err = saveConfig(configBytes)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("City %s successfully removed from favourites\n", city)
+	return nil
+}
+
 func addFavourite(city, country string) {
 	for i := 0; i < len(config.Favourites); i++ {
 		c := config.Favourites[i].City
@@ -191,12 +228,12 @@ func addFavourite(city, country string) {
 		}
 	}
 
-	coordinates, err := fetchCoordinates(city, country)
+	locationData, err := fetchLocationData(city, country)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	newFavourites := append(config.Favourites, Location{City: city, Country: country, Coordinates: coordinates})
+	coordinates := Coordinates{Lat: locationData.Lat, Lon: locationData.Lon}
+	newFavourites := append(config.Favourites, Location{City: locationData.Name, Country: locationData.Country, Coordinates: coordinates})
 	config.Favourites = newFavourites
 	configBytes, err := json.Marshal(config)
 	if err != nil {
@@ -206,20 +243,33 @@ func addFavourite(city, country string) {
 	if err != nil {
 		log.Fatal("Failed to save config file: ", err)
 	}
-	fmt.Printf("New location %s %s added\n", city, country)
+	fmt.Printf("New location %s, %s added to favourites\n", locationData.Name, locationData.Country)
 }
 
-func getWeatherByCity(city, country string) {
-	coordinates, err := fetchCoordinates(city, country)
+func getWeatherByCity(city, country string) error {
+	data, err := fetchLocationData(city, country)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-
+	coordinates := Coordinates{Lat: data.Lat, Lon: data.Lon}
 	res, err := fetchWeather(coordinates)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	printResult(res)
+	return nil
+}
+
+func printCommands() {
+	w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
+	fmt.Fprint(w, "\n-------Commands----------------------------------\n\n")
+	fmt.Fprintln(w, "w\t<City>\t[<Country>]\t|\tGet weather by city")
+	fmt.Fprintln(w, "f\t\t\t|\tGet weather for all of the cities in your favourites")
+	fmt.Fprintln(w, "list\t\t\t|\tList favourites")
+	fmt.Fprintln(w, "fav\t<City>\t[<Country>]\t|\tAdd city to favourites")
+	fmt.Fprintln(w, "remove\t<City>\t\t|\tRemove city from favourites")
+	fmt.Fprintln(w, "help\t\t\t|\tList available commands")
+	fmt.Fprintln(w, "\n---------------------------------------------")
 }
 
 func main() {
@@ -228,22 +278,65 @@ func main() {
 		log.Fatal(err)
 	}
 
+	printCommands()
+
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
-		fmt.Println("Get weather by City")
-		fmt.Print("City: ")
-		var city string = ""
-		var country string = ""
+		fmt.Print("\nCommand: ")
 
 		if scanner.Scan() {
 			input := strings.Fields(scanner.Text())
-			city = input[0]
-			if len(input) > 1 {
-				country = input[1]
+			if len(input) == 0 {
+				continue
 			}
-			getWeatherByCity(city, country)
-			continue
+
+			command := strings.ToLower(input[0])
+
+			var city, country string = "", ""
+
+			if len(input) > 1 {
+				city = strings.ToLower(input[1])
+			}
+			if len(input) > 2 {
+				country = strings.ToLower(input[2])
+			}
+			fmt.Println()
+
+			switch command {
+			case "w":
+				if len(input) < 2 {
+					fmt.Println("Missing city parameter")
+					continue
+				}
+				err := getWeatherByCity(city, country)
+				if err != nil {
+					fmt.Println(err)
+				}
+			case "f":
+				err := fetchFavourites()
+				if err != nil {
+					fmt.Println(err)
+				}
+			case "list":
+				listFavourites()
+			case "fav":
+				if len(input) < 2 {
+					fmt.Println("Missing city parameter")
+					continue
+				}
+				addFavourite(city, country)
+			case "remove":
+				err := removeFavourite(city)
+				if err != nil {
+					fmt.Println(err)
+				}
+			case "help":
+				printCommands()
+			default:
+				fmt.Println("Unknown command")
+			}
+
 		}
 		if err := scanner.Err(); err != nil {
 			log.Fatal("Failed to scan input", scanner.Err())
