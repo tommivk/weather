c173ac77614176812c6108e6b2fff054cc734c25
tommivk <tommikokk@gmail.com>
2023-06-28 12:19:41 +0300

    add config file and favourites

--- main.go
+++ main.go
@@ -16,16 +16,18 @@ import (
 
 var API_KEY = os.Getenv("API_KEY")
 
-func fetchData(url string) ([]byte, error) {
-	res, err := http.Get(url)
-	if err != nil {
-		return nil, errors.New("Failed to fetch")
-	}
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, errors.New("Failed to read response")
-	}
-	return body, nil
+var config Config
+
+type Config struct {
+	Language   string
+	Units      string
+	Favourites []Location
+}
+
+type Location struct {
+	City        string
+	Country     string
+	Coordinates Coordinates
 }
 
 type GeoResponse struct {
@@ -38,6 +40,18 @@ type GeoResponse struct {
 type Coordinates struct {
 	Lat float32
 	Lon float32
+}
+
+func fetchData(url string) ([]byte, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to fetch data: %s", err)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read response: %s", err)
+	}
+	return body, nil
 }
 
 func fetchCoordinates(city, country string) (Coordinates, error) {
@@ -55,7 +69,7 @@ func fetchCoordinates(city, country string) (Coordinates, error) {
 	}
 
 	coordinates := Coordinates{Lat: result[0].Lat, Lon: result[0].Lon}
-	return coordinates, err
+	return coordinates, nil
 }
 
 type WeatherDetails struct {
@@ -87,7 +101,7 @@ type WeatherResult struct {
 }
 
 func fetchWeather(coordinates Coordinates) (WeatherResult, error) {
-	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&units=metric&lang=fi&appid=%s", coordinates.Lat, coordinates.Lon, API_KEY)
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&units=metric&lang=%s&appid=%s", coordinates.Lat, coordinates.Lon, config.Language, API_KEY)
 	data, err := fetchData(url)
 	if err != nil {
 		return WeatherResult{}, err
@@ -118,25 +132,84 @@ func printResult(result WeatherResult) {
 	fmt.Println("--------------------------------------------------------")
 }
 
-func main() {
-	var city string
-	var country string
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		if scanner.Scan() {
-			input := strings.Fields(scanner.Text())
-			if len(input) != 2 {
-				fmt.Println("Invalid input")
-				continue
-			}
-			city = input[0]
-			country = input[1]
-			break
+func fetchFavourites() {
+	if len(config.Favourites) == 0 {
+		fmt.Println("No favourites added")
+	}
+	for i := 0; i < len(config.Favourites); i++ {
+		weather, err := fetchWeather(config.Favourites[i].Coordinates)
+		if err != nil {
+			log.Fatal(nil)
 		}
-		log.Fatal("Failed to scan input")
+		printResult(weather)
+	}
+}
+
+func readConfigFile() error {
+	data, err := os.ReadFile("config.json")
+	if os.IsNotExist(err) {
+		fmt.Println("Config file missing")
+		err = createNewConfigFile()
+		if err != nil {
+			return err
+		}
+	} else if err != nil {
+		return err
 	}
 
+	json.Unmarshal(data, &config)
+	return nil
+}
+
+func createNewConfigFile() error {
+	file, err := json.Marshal(Config{Units: "metric", Language: "en", Favourites: []Location{}})
+	if err != nil {
+		return err
+	}
+	err = saveConfig(file)
+	if err != nil {
+		return err
+	}
+	fmt.Println("New Config file created")
+	return nil
+}
+
+func saveConfig(config []byte) error {
+	err := os.WriteFile("config.json", config, 0666)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func addFavourite(city, country string) {
+	for i := 0; i < len(config.Favourites); i++ {
+		c := config.Favourites[i].City
+		if strings.ToLower(c) == strings.ToLower(city) {
+			fmt.Printf("%s already exists in favourites\n", city)
+			return
+		}
+	}
+
+	coordinates, err := fetchCoordinates(city, country)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	newFavourites := append(config.Favourites, Location{City: city, Country: country, Coordinates: coordinates})
+	config.Favourites = newFavourites
+	configBytes, err := json.Marshal(config)
+	if err != nil {
+		log.Fatal("Failed to marshal data: ", err)
+	}
+	err = saveConfig(configBytes)
+	if err != nil {
+		log.Fatal("Failed to save config file: ", err)
+	}
+	fmt.Printf("New location %s %s added\n", city, country)
+}
+
+func getWeatherByCity(city, country string) {
 	coordinates, err := fetchCoordinates(city, country)
 	if err != nil {
 		log.Fatal(err)
@@ -147,4 +220,34 @@ func main() {
 		log.Fatal(err)
 	}
 	printResult(res)
+}
+
+func main() {
+	err := readConfigFile()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Println("Get weather by City")
+		fmt.Print("City: ")
+		var city string = ""
+		var country string = ""
+
+		if scanner.Scan() {
+			input := strings.Fields(scanner.Text())
+			city = input[0]
+			if len(input) > 1 {
+				country = input[1]
+			}
+			getWeatherByCity(city, country)
+			continue
+		}
+		if err := scanner.Err(); err != nil {
+			log.Fatal("Failed to scan input", scanner.Err())
+		}
+	}
+
 }
